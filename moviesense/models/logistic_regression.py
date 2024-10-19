dedb3852f42523ed8583b7e6ec6d2159293c25d5
anj0la <anjola.aina@gmail.com>
2024-10-18 18:44:22 -0600

    Added label encoder, firstly trained model with 20 epochs and lr of 0.1, and generated figure.

--- moviesense/models/logistic_regression.py
+++ moviesense/models/logistic_regression.py
@@ -22,9 +22,9 @@ class LogisiticRegression:
         - weights - the parameters of the model, adjusted to get the desired probability
         - w0 - an extra parameter, adjusted to get the desired probability
     """
-    def __init__(self, learning_rate: float = 0.1, n_iterations: int = 10) -> None:
-        self.learning_rate = learning_rate
-        self.n_iterations = n_iterations
+    def __init__(self, lr: float = 0.1, epochs: int = 100) -> None:
+        self.lr = lr
+        self.epochs = epochs
         self.weights = None
         self.bias = None # Bias
     
@@ -45,29 +45,37 @@ class LogisiticRegression:
         """
         self._initialize_weights(X_train.shape[1])
         all_train_losses = []
+        # best_loss = []
         print('Starting training...')
                 
-        for epoch in range(self.n_iterations):
+        for epoch in range(self.epochs):
             # Forward pass (make a prediction)
             y_hat = self._forward_pass(X_train)
             
             print('WE MADE A FORWARD PASS!')
             
+            #print(f'type of y_train: {type(y_train)}, type of y_hat: {type(y_hat)}')
+            #print(f'y_train[:5]: {y_train[:5]}, y_hat[:5]: {y_hat[:5]}')
+            #print(f'y_train type: {y_train.dtype}, y_hat type: {y_hat.dtype}')
+
             # Calculate the loss
             loss = self._loss_function(y_train, y_hat)
             
+            print('WE CALCLUATED THE LOSS!')
+            
             # Backward pass and update weights
-            self._update_weights(X_train, y_hat)
+            self._update_weights(X_train, y_train, y_hat)
+            
+            print('WE UPDATED THE WEIGHTS!')
                 
             # Print loss metrics
-            if epoch % 2 == 0:
-                print(f'Iteration {epoch} / {self.n_iterations} with current loss: {loss}')
-                print(f'Weights : {self.weights}')
+            print(f'Epoch {epoch + 1} / {self.epochs}\nCurrent loss: {loss}')
+            # print(f'Weights : {self.weights}')
             
             all_train_losses.append(loss)
             
         # Visualize (and save) plot representing the loss with respect to the epochs
-        self._plot_graph(list(range(self.n_iterations)), all_train_losses)  
+        self._plot_graph(list(range(1, self.epochs + 1)), all_train_losses, self.lr)  
         
     def _forward_pass(self, x):
         z = self.bias + np.dot(x, self.weights)
@@ -75,14 +83,14 @@ class LogisiticRegression:
         
     def _update_weights(self, X, y, y_hat):
         m = X.shape[0]
-        d_weight = (1/m) * np.dot(X.T, (y_hat - y))
-        d_bias = (1/m) * np.sum(y_hat - y)
-        self.weights -= self.learning_rate * d_weight
-        self.bias -= self.learning_rate * d_bias
+        d_weight = (1 / m) * np.dot(X.T, (y_hat - y))
+        d_bias = (1 / m) * np.sum(y_hat - y)
+        self.weights -= self.lr * d_weight
+        self.bias -= self.lr * d_bias
             
-    def _loss_function(self, y, y_hat):
-        m = y.shape[0]
-        return -(1/m) * np.sum(y * np.log(y_hat) + (1 - y) * np.log(1 - y_hat))
+    def _loss_function(self, y_train, y_hat):
+        m = y_train.shape[0]
+        return -(1 / m) * np.sum(y_train * np.log(y_hat) + (1 - y_train) * np.log(1 - y_hat))
     
     def predict(self, X):
         """
@@ -91,10 +99,10 @@ class LogisiticRegression:
         and values less than 0.5 (exclusive) are rounded down to 0.
 
         Args:
-            X (ndarray): the input to make a prediction on
+            X (ndarray): The input to make a prediction on.
 
         Returns:
-            int: the predicted probability of the input (either 0 or 1)
+            int: The predicted probability of the input (either 0 or 1).
         """
         z = np.dot(X, self.weights) + self.bias
         return np.round(self._sigmoid(z))
@@ -109,14 +117,14 @@ class LogisiticRegression:
         Implements the sigmoid function, an activiation function that changes the weighted sum z to a probability.
 
         Args:
-            z (Any): the weighted sum
+            z (Any): The weighted sum.
 
         Returns:
             Any: the predicted probability of z (either 0 or 1)
         """
         return 1 / (1 + np.exp(-z))
     
-    def _plot_graph(self, list_epochs, list_total_loss):
+    def _plot_graph(self, list_epochs, list_total_loss, lr):
         """
         This function plots a graph that visualizes how the loss decreases over the epochs. That is, as the epochs increase, the loss decreases.
 
@@ -128,23 +136,25 @@ class LogisiticRegression:
         ax.plot(list_epochs, list_total_loss) 
         ax.set_xlabel('Number of epochs')
         ax.set_ylabel('Total loss')
-        ax.set_title('Binary Cross Entropy Loss Function as a Function of Epochs')
-        plt.savefig('logistic_regression_loss.png')
-        plt.show()
+        ax.set_title('Loss as a Function of Epochs')
+        plt.savefig(f'moviesense/figures/loss_epoch_{len(list_epochs)}_lr_{lr}.png')
+        # plt.show()
         
 # Running the classifier
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 
 vectorizer = CountVectorizer()
+le = LabelEncoder()
 
 df = pd.read_csv('moviesense/data/cleaned_movie_reviews.csv')
 X = vectorizer.fit_transform(df['review'])
-y = df['sentiment']
+y = le.fit_transform(df['sentiment'].values)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-X_train, X_test = X_train.toarray(), X_test.toarray()
+X_train = X_train.toarray()
 
 print(type(X_train), type(y_train))
 print(X_train.shape)
