dbfc8ad4b2be56847867f02d316d02afff4bec05
anj0la <anjola.aina@gmail.com>
2024-10-27 20:36:04 -0600

    Created function to generate context target pairs.

--- moviesense/models/cbow.py
+++ moviesense/models/cbow.py
@@ -31,8 +31,39 @@ def tokenize_and_build_vocab(text: pd.Series, min_token_length: int = 2) -> dict
 
     return tokenized_sentence, vocab
 
+def generate_context_target_pairs(text: list, vocab: dict, window_size: int = 2) -> list:
+    """
+    Generates context-target pairs for the CBOW algorithm, converting each word to its vocabulary index.
+
+    Args:
+        text (list): List of tokenized sentences, where each sentence is a list of words.
+        vocab (dict): The vocabulary with token-to-index mappings.
+        window_size (int, optional): The context window size. Defaults to 2.
+
+    Returns:
+        list: Generated context-target pairs with indices for model input.
+    """
+    # Generate context-target pairs
+    context_target_pairs = []
+    unk_index = len(vocab)
+    
+    for sentence in text:
+        # Convert each word in the tokenized sentence to a sequence of indices
+        sequence = [vocab.get(word, unk_index) for word in sentence]
+        
+        # Generate context-target pairs from the indexed sequence
+        for i, target in enumerate(sequence):
+            # Get context indices around target word
+            context = sequence[max(0, i - window_size): i] + sequence[i + 1: min(len(sequence), i + window_size + 1)]
+            context_target_pairs.append((context, target))
+    
+    return context_target_pairs
+
 df = pd.read_csv('moviesense/data/reviews/cleaned_movie_reviews.csv')
 
 print(type(df['review']))
 
 tokenized_data, vocab = tokenize_and_build_vocab(df['review'])
+X = generate_context_target_pairs(tokenized_data, vocab)
+
+print(X[:1])
