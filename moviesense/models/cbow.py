8450af072021a59b184c86c868641ff9e29ae5c2
anj0la <anjola.aina@gmail.com>
2024-10-28 01:03:08 -0600

    Moved cbow preprocessing into separate file and constructed cbow model.

--- moviesense/models/cbow.py
+++ moviesense/models/cbow.py
@@ -1,69 +1,22 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader, TensorDataset
-import regex as re
-import pandas as pd
 
-def tokenize_and_build_vocab(text: pd.Series, min_token_length: int = 2) -> dict:
-    """
-    Tokenizes text and builds the vocabulary with the specified minimum token length. It is assumed that the text has been preprocessed.
-
-    Args:
-        text (list): The processed text containing all reviews.
-        min_token_length (int): Minimum length of tokens to keep in the vocabulary. Default is 2.
-
-    Returns:
-        dict: The resulting vocabulary built from the unique tokens in the text.
-    """
-    tokenized_sentence = []
-    for sentence in text: 
-        # Split the sentence into words and add to tokenized_sentence list
-        tokenized_sentence.append(sentence.split())
+class CBOW(nn.Module):
+    def __init__(self, vocab_size: int, hidden_size: int = 128, embedding_dim: int = 100):
+        super(CBOW, self).__init__()
         
-    # Flatten the list of tokenized sentences and filter out single characters if needed
-    all_tokens = [token for sentence in tokenized_sentence for token in sentence if len(token) >= min_token_length]
-    unique_tokens = set(all_tokens)
+        # Embedding Layer
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
 
-    # Create the vocabulary (mapping tokens to indices)
-    vocab = {token: idx for idx, token in enumerate(unique_tokens)}
-    vocab = {**vocab, '<pad>': len(vocab) + 1, '<unk>': len(vocab) + 2}
+        # Multi-Layer Perceptron (MLP)
+        self.hidden = nn.Linear(embedding_dim, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc = nn.Linear(hidden_size, vocab_size) 
 
-    return tokenized_sentence, vocab
+    def forward(self, x: torch.Tensor):
+        out = self.embedding(x)  
+        out = out.mean(dim=1)  
+        out = self.relu(self.hidden(out))
+        out = self.fc(out)
 
-def generate_context_target_pairs(text: list, vocab: dict, window_size: int = 2) -> list:
-    """
-    Generates context-target pairs for the CBOW algorithm, converting each word to its vocabulary index.
-
-    Args:
-        text (list): List of tokenized sentences, where each sentence is a list of words.
-        vocab (dict): The vocabulary with token-to-index mappings.
-        window_size (int, optional): The context window size. Defaults to 2.
-
-    Returns:
-        list: Generated context-target pairs with indices for model input.
-    """
-    # Generate context-target pairs
-    context_target_pairs = []
-    unk_index = len(vocab)
-    
-    for sentence in text:
-        # Convert each word in the tokenized sentence to a sequence of indices
-        sequence = [vocab.get(word, unk_index) for word in sentence]
-        
-        # Generate context-target pairs from the indexed sequence
-        for i, target in enumerate(sequence):
-            # Get context indices around target word
-            context = sequence[max(0, i - window_size): i] + sequence[i + 1: min(len(sequence), i + window_size + 1)]
-            context_target_pairs.append((context, target))
-    
-    return context_target_pairs
-
-df = pd.read_csv('moviesense/data/reviews/cleaned_movie_reviews.csv')
-
-print(type(df['review']))
-
-tokenized_data, vocab = tokenize_and_build_vocab(df['review'])
-X = generate_context_target_pairs(tokenized_data, vocab)
-
-print(X[:1])
+        return out
---
+++ moviesense/utils/cbow_preprocess.py
@@ -0,0 +1,90 @@
+from torch.utils.data import DataLoader
+import pandas as pd
+
+def tokenize_and_build_vocab(text: pd.Series, min_token_length: int = 2) -> dict:
+    """
+    Tokenizes text and builds the vocabulary with the specified minimum token length. It is assumed that the text has been preprocessed.
+
+    Args:
+        text (list): The processed text containing all reviews.
+        min_token_length (int): Minimum length of tokens to keep in the vocabulary. Default is 2.
+
+    Returns:
+        dict: The resulting vocabulary built from the unique tokens in the text.
+    """
+    tokenized_sentence = []
+    for sentence in text: 
+        # Split the sentence into words and add to tokenized_sentence list
+        tokenized_sentence.append(sentence.split())
+        
+    # Flatten the list of tokenized sentences and filter out single characters if needed
+    all_tokens = [token for sentence in tokenized_sentence for token in sentence if len(token) >= min_token_length]
+    unique_tokens = set(all_tokens)
+
+    # Create the vocabulary (mapping tokens to indices)
+    vocab = {token: idx for idx, token in enumerate(unique_tokens)}
+    vocab = {**vocab, '<pad>': len(vocab) + 1, '<unk>': len(vocab) + 2}
+
+    return tokenized_sentence, vocab
+
+def generate_context_target_pairs(text: list, vocab: dict, window_size: int = 2) -> list:
+    """
+    Generates context-target pairs for the CBOW algorithm, converting each word to its vocabulary index.
+
+    Args:
+        text (list): List of tokenized sentences, where each sentence is a list of words.
+        vocab (dict): The vocabulary with token-to-index mappings.
+        window_size (int, optional): The context window size. Defaults to 2.
+
+    Returns:
+        list: Generated context-target pairs with indices for model input.
+    """
+    # Generate context-target pairs
+    context_target_pairs = []
+    unk_index = len(vocab)
+    
+    for sentence in text:
+        # Convert each word in the tokenized sentence to a sequence of indices
+        sequence = [vocab.get(word, unk_index) for word in sentence]
+        
+        # Generate context-target pairs from the indexed sequence
+        for i, target in enumerate(sequence):
+            # Get context words within the window, excluding the target
+            left_context = sequence[max(0, i - window_size): i]
+            right_context = sequence[i + 1: i + window_size + 1]
+            context_words = left_context + right_context
+            
+            # Test -> context_words is always at least of length 1
+            if len(context_words) < 1:
+                print('empty')
+            
+            # Create individual (context_word, target) pairs
+            for context_word in context_words:
+                context_target_pairs.append((context_word, target))
+    
+    return context_target_pairs
+
+window_size = 2
+
+df = pd.read_csv('moviesense/data/reviews/cleaned_movie_reviews.csv')
+
+# print(type(df['review']))
+
+tokenized_data, vocab = tokenize_and_build_vocab(df['review'])
+data = generate_context_target_pairs(tokenized_data, vocab)
+
+print(data[:10])
+
+dataframe = pd.DataFrame(data, columns=['context', 'target'])
+
+# Convert pairs to tensors for PyTorch
+#context_target_pairs_tensors = [(torch.tensor(context, dtype=torch.long), torch.tensor(target, dtype=torch.long))
+#                                for context, target in data]
+
+
+#batch_size = 64  # You can adjust this value
+#dataloader = DataLoader(context_target_pairs_tensors, batch_size=batch_size, shuffle=True)
+
+
+
+
